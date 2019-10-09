6166661eb293453c6d55ad6500f9fc85afd2e56a
Azenstrange <edouard.eisenfisz@hetic.net>
2019-10-09 11:55:13 +0100

    grand

--- modele/scripts/main.js
+++ modele/scripts/main.js
@@ -3,35 +3,58 @@ const element2 = document.querySelector('.element2')
 const element3 = document.querySelector('.element3')
 const element4 = document.querySelector('.element4')
 const element5 = document.querySelector('.element5')
+const tableau = document.querySelector('.tableau')
+const img1 = document.querySelector('.img1')
+const img2 = document.querySelector('.img2')
+const img3 = document.querySelector('.img3')
+const img4 = document.querySelector('.img4')
+const img5 = document.querySelector('.img5')
+const lesimg=document.querySelector('.lesimg')
+let imglist = [img1, img2, img3, img4, img5]
 
+element1.addEventListener(
+  'click',
+  function() {
+  tableau.classList.toggle("invisible")
+  img1.classList.remove("invisible")
+  }
+)
 element2.addEventListener(
   'click',
   function() {
-  console.log("Hello World")
+    tableau.classList.add("invisible")
+    img2.classList.remove("invisible")
   }
 )
 element3.addEventListener(
   'click',
   function() {
-  console.log("Hello World")
+    tableau.classList.toggle("invisible")
+    img3.classList.remove("invisible")
   }
 )
 element4.addEventListener(
   'click',
   function() {
-  console.log("Hello World")
-  }
-)
-element5.addEventListener(
-  'click',
-  function() {
-  console.log("Hello World")
+    tableau.classList.toggle("invisible")
+    img4.classList.remove("invisible")
   }
 )
 
-element1.addEventListener(
+element5.addEventListener(
   'click',
   function() {
-  console.log("Hello World")
+    tableau.classList.toggle("invisible")
+    img5.classList.remove("invisible")
+  }
+)
+lesimg.addEventListener(
+  'click', function() {
+      for (var i = 0; i < imglist.length; i++) {
+        if(!imglist[i].classList.contains("invisible")){
+            imglist[i].classList.toggle("invisible")
+        }
+      }
+      tableau.classList.toggle("invisible")
   }
 )
